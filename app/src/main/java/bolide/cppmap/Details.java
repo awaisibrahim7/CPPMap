d66df1b7835d749a85ae4748692f483c5c631de3
Awais Ibrahim <Awais Ibrahim>
2017-05-01 03:58:33 -0700

    Initial commit

---
+++ app/src/main/java/bolide/cppmap/Details.java
@@ -0,0 +1,26 @@
+package bolide.cppmap;
+
+/**
+ * Created by Awais on 5/1/2017.
+ */
+//Contains the descriptions for each of the markers.
+public class Details {
+    String bldg1 = "Holds the Departments of Communication, Economics, and Philosophy. It also\n" +
+            "contains the home of IT Services and the Kellogg Honors College.";
+    String bldg2 = "Has the Departments of Agribusiness & Food Industry Management/Agricultural\n" +
+            "Science, Animal & Veterinary Sciences, Apparel Merchandising & Management, \n" +
+            "Human Nutrition & Food Science and Plant Science.";
+    String bldg3 = "Contains classrooms, laboratories and the College of Science Advising Center.";
+    String bldg4 = "Holds Science Laboratories along with classrooms as well.";
+    String bldg5 = "Home of the Departments of English and Foreign Language, Geography and\n" +
+            "Anthropology, and Psychology and Sociology.";
+    String bldg6 = "Houses the Departments of Ethnic and Women's Studies, Education, \n" +
+            "Interdisciplinary General Education(IGE), and Liberal Studies.";
+    String bldg7 = "The College of Environmental Design offers undergraduate and graduate degrees\n" +
+            "in Architecture, Landscape Architecture, and Urban and Regional Planning;\n" +
+            "undergraduate degrees in Art History and Graphic Design; and a graduate degree \n" +
+            "in Regenerative Studies.";
+    String bldg8 = "Home to the departments of Biological Sciences, Chemistry, Biochemistry, \n" +
+            "Computer Science, Geological Sciences, Kinesiology and Health Promotion,\n" +
+            "Math and Statistics, as well as the Physics and Astronomy Department.";
+}
